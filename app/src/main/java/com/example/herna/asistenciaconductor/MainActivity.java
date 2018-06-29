f865ce495e8ad6a1a007c1d4fb349fa6e277f089
Hernán Mansilla <Hernan Mansilla@fulmar.local>
2018-06-29 16:30:49 -0300

    Version 14
    - GPS andando con algunos bugs

--- app/src/main/java/com/example/herna/asistenciaconductor/MainActivity.java
+++ app/src/main/java/com/example/herna/asistenciaconductor/MainActivity.java
@@ -11,7 +11,10 @@ package com.example.herna.asistenciaconductor;
 //https://medium.com/@victor.garibayy/obteniendo-mi-ubicaci%C3%B3n-en-android-studio-377226910823
 //https://medium.com/@victor.garibayy/obteniendo-mi-ubicaci%C3%B3n-en-android-studio-377226910823
 
+//https://www.youtube.com/watch?v=ipr7Y7lcYgA
+
 import android.Manifest;
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -29,6 +32,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -56,36 +60,37 @@ import static com.example.herna.asistenciaconductor.AsyncTask_BT_RX.Datos_Recibi
 import static com.example.herna.asistenciaconductor.AsyncTask_BTinit_Dialog.connectedThread;
 import static com.example.herna.asistenciaconductor.Ubicacion.Latitud_GPS;
 import static com.example.herna.asistenciaconductor.Ubicacion.Longitud_GPS;
+import static com.example.herna.asistenciaconductor.Ubicacion.LocationManager;
+import static com.example.herna.asistenciaconductor.Ubicacion.lc;
 
-public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback
-{
-   private ArrayList<DatosRecyclerViewPrincipal> ListaUsuariosPrincipal;
-   private RecyclerView recyclerUsuarios;
-   private static final int ENABLE_BLUETOOTH = 1;
-   private static final int SOLICITA_CONEXION =2;
-   static public String MAC = null;
-   static public BluetoothDevice mDevice = null;
-   BluetoothAdapter mBluetoothAdapter = null;
-   AsyncTask_BTinit_Dialog Bluetooth_init;
-  // static public AsyncTask_BT_RX Bluetooth_RX;
-   static public boolean Bluetooth_Conectado=false;
-   static public boolean Bluetooth_Encendido=false;
-   static public byte[] buffer_rx_BT = new byte[1];  // buffer store for the stream
-    static public int Cant_bytes_rx_BT=0; // bytes returned from read()
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<DatosRecyclerViewPrincipal> ListaUsuariosPrincipal;
+    private RecyclerView recyclerUsuarios;
+    private static final int ENABLE_BLUETOOTH = 1;
+    private static final int SOLICITA_CONEXION = 2;
+    static public String MAC = null;
+    static public BluetoothDevice mDevice = null;
+    BluetoothAdapter mBluetoothAdapter = null;
+    AsyncTask_BTinit_Dialog Bluetooth_init;
+    // static public AsyncTask_BT_RX Bluetooth_RX;
+    static public boolean Bluetooth_Conectado = false;
+    static public boolean Bluetooth_Encendido = false;
+    static public byte[] buffer_rx_BT = new byte[1];  // buffer store for the stream
+    static public int Cant_bytes_rx_BT = 0; // bytes returned from read()
     static public UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
     static public ProgressDialog pDialog;
-   NotificationCompat.Builder mBuilder;
-    static final int NOTIF_ALERTA_ID=1;
+    NotificationCompat.Builder mBuilder;
+    static final int NOTIF_ALERTA_ID = 1;
     EditText Latitud_editText;
     EditText Longitud_editText;
-    boolean gps_enabled = false,network_enabled = false;
-   public Button Boton_GPS;
-   static public int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION=1;
-   static public Context contexto_gral;
+    boolean gps_enabled = false, network_enabled = false;
+    public Button Boton_GPS;
+    static public int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    static public Context contexto_gral;
+    static public Ubicacion ubicacion;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -100,51 +105,8 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         LlenarUsuarios();
 
         contexto_gral = getApplicationContext();
-        Ubicacion ubicacion = new Ubicacion(this);
+        ubicacion = new Ubicacion(this);
 
-   /*      if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        //if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-            Toast.makeText(MainActivity.this, "No tiene permisos para usar el GPS ", Toast.LENGTH_SHORT).show();
-           // return;
-        }
-        else
-            {
-                LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
-
-        }
-*/
-      //  Ubicacion ubicacion = new Ubicacion(this);
-
-     //   LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-     //   Localizacion Local = new Localizacion();
-
-    /*    try
-        {
-            gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        }
-        catch(Exception ex)
-        {
-            try
-            {
-                network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-            }
-            catch(Exception ex2)
-            {
-                Toast toast = Toast.makeText(getApplicationContext(),"Ubicaci�n no disponible",Toast.LENGTH_SHORT);
-                toast.show();
-
-            }
-
-        }
-*/
         // Registramos el BroadcastReceiver que instanciamos previamente para
         // detectar los distintos eventos que queremos recibir
         IntentFilter filtro = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
@@ -152,16 +114,16 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
         mBuilder = new NotificationCompat.Builder(MainActivity.this);
         mBuilder.setAutoCancel(true);
-   //     mBuilder.setSmallIcon(R.mipmap.ic_launcher);
-   //     mBuilder.setPriority(Notification.PRIORITY_HIGH);
+        //     mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        //     mBuilder.setPriority(Notification.PRIORITY_HIGH);
         mBuilder.setSmallIcon(android.R.drawable.stat_sys_warning);
         mBuilder.setTicker("Datos Recibidos Bluetooth");
         mBuilder.setWhen(System.currentTimeMillis());
-    //    mBuilder.setLargeIcon((((BitmapDrawable)getResources();
-    //    mBuilder.getDrawable(R.drawable.ic_launcher_foreground)).getBitmap()));
+        //    mBuilder.setLargeIcon((((BitmapDrawable)getResources();
+        //    mBuilder.getDrawable(R.drawable.ic_launcher_foreground)).getBitmap()));
         mBuilder.setContentTitle("Datos Recibidos Bluetooth");
         mBuilder.setContentText("Ejemplo de notificación.");
-     //   mBuilder.setContentInfo("4");
+        //   mBuilder.setContentInfo("4");
 
         Intent Intent = new Intent(MainActivity.this, MainActivity.class);
 
@@ -173,63 +135,52 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
         AdaptadorRecyclerViewPrincipal adapter = new AdaptadorRecyclerViewPrincipal(ListaUsuariosPrincipal);
 
-        adapter.setOnItemClickListener(new AdaptadorRecyclerViewPrincipal.OnItemClickListener()
-        {
+        adapter.setOnItemClickListener(new AdaptadorRecyclerViewPrincipal.OnItemClickListener() {
             @Override
-            public void onItemClick(int position)
-            {
-                    BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+            public void onItemClick(int position) {
+                BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
 
-                    if (!bluetooth.isEnabled())
-                    {
-                        // Si el Bluetooth no esta activado, pregunto si quiero activarlo
-                        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        startActivityForResult(intent, ENABLE_BLUETOOTH);
+                if (!bluetooth.isEnabled()) {
+                    // Si el Bluetooth no esta activado, pregunto si quiero activarlo
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intent, ENABLE_BLUETOOTH);
 
+                } else {
+                    Bluetooth_Encendido = true;
+
+                    if (Bluetooth_Conectado == false) {
+                        Intent activityListaDispositivos = new Intent(MainActivity.this, ListaDispositivos.class);
+                        startActivityForResult(activityListaDispositivos, SOLICITA_CONEXION);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Ya posee una conexion activa", Toast.LENGTH_SHORT).show();
                     }
-                    else
-                    {
-                        Bluetooth_Encendido = true;
-
-                        if(Bluetooth_Conectado == false)
-                        {
-                            Intent activityListaDispositivos = new Intent(MainActivity.this, ListaDispositivos.class);
-                            startActivityForResult(activityListaDispositivos, SOLICITA_CONEXION);
-                        }
-                        else
-                        {
-                            Toast.makeText(MainActivity.this, "Ya posee una conexion activa", Toast.LENGTH_SHORT).show();
-                        }
-                    }
+                }
             }
+
             @Override
-            public void onDeleteClick(int position)
-            {
-                switch (position)
-                {
+            public void onDeleteClick(int position) {
+                switch (position) {
                     case 0:
-                     if (Bluetooth_Conectado == true && Bluetooth_Encendido == true) {
-                        connectedThread.enviar_string("hola");
+                        if (Bluetooth_Conectado == true && Bluetooth_Encendido == true) {
+                            connectedThread.enviar_string("hola");
 
-                        Toast.makeText(MainActivity.this, "Envie dato", Toast.LENGTH_SHORT).show();
-                    } else
-                        {
+                            Toast.makeText(MainActivity.this, "Envie dato", Toast.LENGTH_SHORT).show();
+                        } else {
                             mNotificationManager.notify(NOTIF_ALERTA_ID, mBuilder.build());
-                         Toast.makeText(MainActivity.this, "No posee una conexion Bluetooth " + position, Toast.LENGTH_SHORT).show();
-                     }
+                            Toast.makeText(MainActivity.this, "No posee una conexion Bluetooth " + position, Toast.LENGTH_SHORT).show();
+                        }
 
-                    break;
+                        break;
 
                     case 1:
 
-                        if(Datos_Recibidos_BT == true)
-                        {
-                         //   String datos_recibidos = Arrays.toString(buffer_rx_BT);
+                        if (Datos_Recibidos_BT == true) {
+                            //   String datos_recibidos = Arrays.toString(buffer_rx_BT);
                             mNotificationManager.notify(NOTIF_ALERTA_ID, mBuilder.build());
                             String datos_recibidos = new String(buffer_rx_BT);
                             Toast.makeText(MainActivity.this, "Datos recibidos: " + datos_recibidos, Toast.LENGTH_SHORT).show();
                             Datos_Recibidos_BT = false;
-                            Cant_bytes_rx_BT=0;
+                            Cant_bytes_rx_BT = 0;
                         }
                         break;
                 }
@@ -241,15 +192,36 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         // Linea de separacion entre items de la lista
         recyclerUsuarios.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
-        Boton_GPS.setOnClickListener(new View.OnClickListener()
-        {
+        Boton_GPS.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Latitud_editText.setText(Latitud_GPS);
                 Longitud_editText.setText(Longitud_GPS);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Por si no estaba encendido el GPS en el momento de iniciar la aplicacion
+        if ((!gps_enabled) && (LocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))) {
+            // Si se encendio inicio los servicios para obtener la posicion
+            gps_enabled = true;
+            // Hago el chequeo de tener permiso para usar el gps
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+                Toast.makeText(contexto_gral, "Sin permiso GPS", Toast.LENGTH_SHORT).show();
+                return;
+            }
+       //     lc = LocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            // Hago la peticion de tomar 1 segundo de muestras de GPS
+       //     LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, ubicacion.onLocationChanged(lc));
+            Toast.makeText(MainActivity.this, "GPS encendido", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // Instanciamos un BroadcastReceiver que se encargara de detectar si el estado
--- app/src/main/java/com/example/herna/asistenciaconductor/Ubicacion.java
+++ app/src/main/java/com/example/herna/asistenciaconductor/Ubicacion.java
@@ -2,6 +2,7 @@ package com.example.herna.asistenciaconductor;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -10,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -24,78 +26,84 @@ import static com.example.herna.asistenciaconductor.MainActivity.contexto_gral;
 public class Ubicacion implements LocationListener
 {
     private Context contexto;
-    LocationManager LocationManager;
+    static public LocationManager LocationManager;
     //String proveedor;
-    private boolean gps_enabled;
-    private boolean network_enabled;
+    static public boolean gps_enabled;
+    static public boolean network_enabled;
     static public String Latitud_GPS=null;
     static public String Longitud_GPS=null;
+    static public Location lc;
+    public PendingIntent onLocationChanged;
 
     public Ubicacion(Context ctx)
     {
         this.contexto = ctx;
         LocationManager = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
 
-/*
-        try
-        {
-            gps_enabled = LocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        }
-        catch(Exception ex)
-        {
-            try
+            if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             {
-                network_enabled = LocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-            }
-            catch(Exception ex2)
-            {
-                Toast.makeText(ctx, "Error al iniciar la ubicacion ", Toast.LENGTH_SHORT).show();
-             //   toast.show();
-
-            }
-        }*/
-        if(checkLocation())
-        {
-            if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                //     if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 ActivityCompat.requestPermissions((Activity) this.contexto, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
 
                 Toast.makeText(ctx, "No tiene permisos para usar el GPS ", Toast.LENGTH_SHORT).show();
                 return;
-            } else {
-                LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+            }
+            else
+                {
+                if(checkLocation())
+                {
+                    if(gps_enabled)
+                    {
+                        // Hago la peticion de tomar 1 segundo de muestras de GPS
+                        LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+                        // Obtengo la ultima posicion valida
+                        lc = LocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    }
+                    else if (network_enabled)
+                    {
+                        // Hago la peticion de tomar 1 segundo de muestras de GPS
+                        LocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, this);
+                        // Obtengo la ultima posicion valida
+                        lc = LocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    }
 
-                if (isLocationEnabled()) {
-                    Location lc = LocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    Latitud_GPS = Double.toString(lc.getLatitude());
-                    Longitud_GPS = Double.toString(lc.getLongitude());
+                    if(lc != null)
+                    {
+                        Latitud_GPS = Double.toString(lc.getLatitude());
+                        Longitud_GPS = Double.toString(lc.getLongitude());
+                    }
 
                     Toast.makeText(ctx, "Ultima posicion valida" + Latitud_GPS, Toast.LENGTH_SHORT).show();
                 }
             }
-        }
     }
 
     @Override
-    public void onLocationChanged(Location location)
+    public void onLocationChanged (Location location)
     {
-        if(checkLocation())
-        {
+        if (checkLocation() && location != null) {
             Latitud_GPS = Double.toString(location.getLatitude());
             Longitud_GPS = Double.toString(location.getLongitude());
         }
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+        switch (status)
+        {
+            case LocationProvider.AVAILABLE:
+           //     Toast.makeText(contexto_gral, "GPS habilitado", Toast.LENGTH_SHORT).show();
+                break;
 
+            case LocationProvider.OUT_OF_SERVICE:
+            //    Toast.makeText(contexto_gral, "GPS deshabilitado", Toast.LENGTH_SHORT).show();
+                break;
+
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+            //    Toast.makeText(contexto_gral, "GPS temporalmente deshabilitado", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 
     @Override
@@ -146,7 +154,9 @@ public class Ubicacion implements LocationListener
 
     private boolean isLocationEnabled()
     {
-        return LocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                LocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        gps_enabled= LocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        network_enabled=LocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        return gps_enabled || network_enabled;
     }
 }
